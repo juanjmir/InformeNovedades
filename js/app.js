bcc9b1b51deb79eb900447b1583b5c5215ba085f
juanjmir <73290977+juanjmir@users.noreply.github.com>
2026-05-19 11:38:30 -0400

    .

--- js/app.js
+++ js/app.js
@@ -175,7 +175,7 @@ document.getElementById("formInforme").addEventListener("submit", async e => {
   try {
     // 3. UN SOLO fetch (Este hace ambas cosas en el servidor)
     await fetch(
-      "https://script.google.com/macros/s/AKfycby9mBxujh2T3pPqeqsY8n4pBvhKU-TojV-U_eWST6olAO-WA_dVN_gDiLGY2fEwAP4T9w/exec",
+      "https://script.google.com/macros/s/AKfycbyxdHnV7jeY7Iy1aG2_P7CcSjXtcE5CEeGv_r1VYtrNV0UXvoXjPcOwSN0J1IsxRB0DVQ/exec",
       {
         method: "POST",
         mode: "no-cors",
